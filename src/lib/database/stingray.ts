0e9ec46a93df9afdbcfe895bfa5d00ef955b6638
Server <sofia@familjenblom.se>
2023-04-21 10:39:47 +0200

    Fix from server

---
+++ src/lib/database/stingray.ts
@@ -0,0 +1,87 @@
+import { getConnectionPool } from "./connection";
+import mysql, { RowDataPacket, OkPacket } from "mysql2/promise";
+export type Stingray = {
+  stingray_id: number;
+  long: number;
+  lat: number;
+  pitch: number;
+  roll: number;
+  time: Date;
+  temperature: number;
+};
+export const createOne = async ({
+  long,
+  lat,
+  pitch,
+  roll,
+  time,
+  temperature,
+}: Stingray) => {
+  const connection = await getConnectionPool();
+  const result = await connection.query(
+    `
+      INSERT INTO stingray (longitude, latitude, pitch, roll, time, temperature)
+      VALUES (?, ?, ?, ?, ?, ?)
+  `,
+    [long, lat, pitch, roll, time, temperature]
+  );
+  const okPacket = result[0] as OkPacket;
+  return okPacket.insertId;
+};
+export const findById = async (id: number) => {
+  const connection = await getConnectionPool();
+  const result = await connection.query(
+    `
+        SELECT *
+        FROM stingray
+        WHERE stingray_id = ?
+    `,
+    [id]
+  );
+  const rows = result[0] as RowDataPacket[];
+  return rows[0] as Stingray;
+};
+export const findMany = async () => {
+  const connection = await getConnectionPool();
+  const result = await connection.query(`
+        SELECT *
+        FROM stingray
+    `);
+  return result[0] as Stingray[];
+};
+export const updateOne = async ({
+  id,
+  long,
+  lat,
+  pitch,
+  roll,
+  time,
+  temperature,
+}: Stingray & { id: number }) => {
+  const connection = await getConnectionPool();
+  const result = await connection.query(
+    `
+      UPDATE stingray
+      SET long   = ?,
+          lat    = ?,
+          pitch       = ?,
+          roll        = ?,
+          time        = ?,
+          temperature = ?
+      WHERE stingray_id = ?
+  `,
+    [long, lat, pitch, roll, time, temperature, id]
+  );
+  return result[0] as OkPacket;
+};
+export const deleteById = async (id: number) => {
+  const connection = await getConnectionPool();
+  const result = await connection.query(
+    `
+      DELETE FROM stingray
+      WHERE stingray_id = ?
+  `,
+    [id]
+  );
+  return result[0] as OkPacket;
+};
