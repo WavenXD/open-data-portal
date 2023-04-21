0956e7d36ea541e69197ae1283ac410452d5a056
WavenXD <artursbiten@gmail.com>
2023-04-21 15:12:07 +0200

    s

--- src/pages/api/v3/moving-stations/measurements.ts
+++ src/pages/api/v3/moving-stations/measurements.ts
@@ -1,4 +1,4 @@
-// pages/api/pitchRollYaw.ts
+// pages/api/measurements.ts
 
 import { NextApiRequest, NextApiResponse } from "next";
 import { createOne, Stingray } from "~/lib/database/stingray";
@@ -13,82 +13,50 @@ interface PitchRollYawData {
   pitch: number;
   roll: number;
   yaw: number;
-  time: Date;
-  long: number;
-  lat: number;
-  temperature: number;
 }
-
 
 // Export default function to handle API requests
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     // Check the HTTP method of the request
-    if (req.method === "GET") {
+    if (req.method === 'GET') {
       // Handle GET request
 
       // Return the current pitch, roll, and yaw data as a JSON response
       res.status(200).json({ pitch, roll, yaw });
-    } else if (req.method === "POST") {
+    } else if (req.method === 'POST') {
       // Handle POST request
 
       // Parse the incoming JSON request body
+      const requestBody: PitchRollYawData = JSON.parse(req.body);
 
-      const requestBody: PitchRollYawData = JSON.parse(req.body) as PitchRollYawData;
-      const { pitch, roll, long, lat, time, temperature } = req.body;
-      // Create a Stingray object using the properties from the request body
-      const stingray: Stingray = {
+      // Store the request data into the database
+      await createOne({
         stingray_id: 0,
+        long: 0, // Update with your own values
+        lat: 0, // Update with your own values
         pitch: requestBody.pitch,
         roll: requestBody.roll,
-        long: 0, // replace with the correct value from requestBody
-        lat: 0, // replace with the correct value from requestBody
-        time: new Date(time), // replace with the correct value from requestBody
-        temperature: 0, // replace with the correct value from requestBody
-      };
-
-      console.log('req.body:', req.body); // TEST
-      // const requestBody: Stingray = {
-      //   stingray_id: 0,
-      //   pitch,
-      //   roll,
-      //   long,
-      //   lat,
-      //   time,
-      //   temperature,
-      // };
+        time: new Date(),
+        temperature: 0, // Update with your own values
+      });
 
       // Update pitch, roll, and yaw data with the values from the request body
-      //pitch = requestBody.pitch;
-      //roll = requestBody.roll;
-      //yaw = requestBody.yaw;
-
-      // Store the data in the database
-      console.log('requestBody:', requestBody);
-      console.log('requestBody.pitch:', requestBody.pitch);
-      console.log('requestBody.roll:', requestBody.roll);
-      console.log('requestBody.yaw:', requestBody.yaw);
-      console.log('requestBody.long:', requestBody.long);
-      console.log('requestBody.lat:', requestBody.lat);
-      console.log('requestBody.time:', requestBody.time);
-      console.log('requestBody.temperature:', requestBody.temperature);
-      const id = await createOne(stingray);
+      pitch = requestBody.pitch;
+      roll = requestBody.roll;
+      yaw = requestBody.yaw;
 
       // Print the received data to console (for demonstration purposes)
-      console.log("Received data from Raspberry Pi:", requestBody);
+      console.log('Received data from Raspberry Pi:', requestBody);
 
       // Return a JSON response indicating success
-      res.status(200).json({
-        success: true,
-        message: "Stingray data received successfully",
-        id,
-      });
+      res.status(200).json({ success: true, message: 'Pitch, roll, and yaw data received successfully' });
     } else {
       // Handle unsupported HTTP methods with an error response
-      res.status(400).json({ success: false, message: "Invalid HTTP method" });
+      res.status(400).json({ success: false, message: 'Invalid HTTP method' });
     }
   } catch (error) {
     // Handle any errors with an error response
-    res.status(500).json({ success: false, message: "Internal server error" });
+    res.status(500).json({ success: false, message: 'Internal server error' });
   }
 };
