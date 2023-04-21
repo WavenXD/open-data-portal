58e53faab2d04490ee7acf9fec7335fd89ac0e16
Server <sofia@familjenblom.se>
2023-04-21 14:15:43 +0200

    Ran over with prettier

--- src/pages/api/v3/moving-stations/measurements.ts
+++ src/pages/api/v3/moving-stations/measurements.ts
@@ -1,6 +1,6 @@
 // pages/api/pitchRollYaw.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 import { createOne, Stingray } from "~/lib/database/stingray";
 
 // Global variables to store pitch, roll, and yaw data
@@ -30,7 +30,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       // Parse the incoming JSON request body
       //const requestBody: PitchRollYawData = JSON.parse(req.body);
       const { pitch, roll, yaw, long, lat, time, temperature } = req.body;
-      const requestBody: Stingray = { stingray_id: 0, pitch, roll, long, lat, time, temperature};
+      const requestBody: Stingray = {
+        stingray_id: 0,
+        pitch,
+        roll,
+        long,
+        lat,
+        time,
+        temperature,
+      };
 
       // Update pitch, roll, and yaw data with the values from the request body
       //pitch = requestBody.pitch;
@@ -44,7 +52,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       console.log("Received data from Raspberry Pi:", requestBody);
 
       // Return a JSON response indicating success
-      res.status(200).json({ success: true, message: 'Stingray data received successfully', id });
+      res.status(200).json({
+        success: true,
+        message: "Stingray data received successfully",
+        id,
+      });
     } else {
       // Handle unsupported HTTP methods with an error response
       res.status(400).json({ success: false, message: "Invalid HTTP method" });
