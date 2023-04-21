5ad7481828e82b638ea60c7a12a445b90544996e
WavenXD <artursbiten@gmail.com>
2023-04-21 15:19:48 +0200

    ooga booga

--- src/pages/api/v3/moving-stations/measurements.ts
+++ src/pages/api/v3/moving-stations/measurements.ts
@@ -12,7 +12,7 @@ let yaw = 0;
 interface PitchRollYawData {
   pitch: number;
   roll: number;
-  yaw: number;
+  //yaw: number;
 }
 
 // Export default function to handle API requests
@@ -23,37 +23,39 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       // Handle GET request
 
       // Return the current pitch, roll, and yaw data as a JSON response
-      res.status(200).json({ pitch, roll, yaw });
+      res.status(200).json({ pitch, roll});
     } else if (req.method === 'POST') {
       // Handle POST request
 
       // Parse the incoming JSON request body
-      const requestBody: PitchRollYawData = JSON.parse(req.body);
+      try {
+        // Parse the incoming JSON request body
+        const requestBody: PitchRollYawData = JSON.parse(req.body);
 
-      // Store the request data into the database
-      await createOne({
-        stingray_id: 0,
-        long: 0, // Update with your own values
-        lat: 0, // Update with your own values
-        pitch: requestBody.pitch,
-        roll: requestBody.roll,
-        time: new Date(),
-        temperature: 0, // Update with your own values
-      });
+        // Store the request data into the database
+        await createOne({
+          stingray_id: 0,
+          long: 0, // Update with your own values
+          lat: 0, // Update with your own values
+          pitch: requestBody.pitch,
+          roll: requestBody.roll,
+          time: new Date(),
+          temperature: 0, // Update with your own values
+        });
 
-      // Update pitch, roll, and yaw data with the values from the request body
-      pitch = requestBody.pitch;
-      roll = requestBody.roll;
-      yaw = requestBody.yaw;
+        // Update pitch, roll, and yaw data with the values from the request body
+        pitch = requestBody.pitch;
+        roll = requestBody.roll;
 
-      // Print the received data to console (for demonstration purposes)
-      console.log('Received data from Raspberry Pi:', requestBody);
+        // Print the received data to console (for demonstration purposes)
+        console.log('Received data from Raspberry Pi:', requestBody);
 
-      // Return a JSON response indicating success
-      res.status(200).json({ success: true, message: 'Pitch, roll, and yaw data received successfully' });
-    } else {
-      // Handle unsupported HTTP methods with an error response
-      res.status(400).json({ success: false, message: 'Invalid HTTP method' });
+        // Return a JSON response indicating success
+        res.status(200).json({ success: true, message: 'Pitch, roll, and yaw data received successfully' });
+      } catch (error) {
+        // Handle any errors with an error response
+        res.status(400).json({ success: false, message: 'Invalid JSON request body' });
+      }
     }
   } catch (error) {
     // Handle any errors with an error response
