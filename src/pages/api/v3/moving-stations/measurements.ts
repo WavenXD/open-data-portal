e45f941d74e1a7dffc51f2d122882de243ecd07a
WavenXD <artursbiten@gmail.com>
2023-04-21 11:30:57 +0200

    push insert into table

--- src/pages/api/v3/moving-stations/measurements.ts
+++ src/pages/api/v3/moving-stations/measurements.ts
@@ -1,6 +1,7 @@
 // pages/api/pitchRollYaw.ts
 
 import { NextApiRequest, NextApiResponse } from 'next';
+import { createOne, Stingray } from "~/lib/database/stingray";
 
 // Global variables to store pitch, roll, and yaw data
 let pitch = 0;
@@ -27,18 +28,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       // Handle POST request
 
       // Parse the incoming JSON request body
-      const requestBody: PitchRollYawData = JSON.parse(req.body);
+      //const requestBody: PitchRollYawData = JSON.parse(req.body);
+      const { pitch, roll, yaw, long, lat, time, temperature } = req.body;
+      const requestBody: Stingray = { stingray_id: 0, pitch, roll, long, lat, time, temperature};
 
       // Update pitch, roll, and yaw data with the values from the request body
-      pitch = requestBody.pitch;
-      roll = requestBody.roll;
-      yaw = requestBody.yaw;
+      //pitch = requestBody.pitch;
+      //roll = requestBody.roll;
+      //yaw = requestBody.yaw;
+
+      // Store the data in the database
+      const id = await createOne(requestBody);
 
       // Print the received data to console (for demonstration purposes)
       console.log('Received data from Raspberry Pi:', requestBody);
 
       // Return a JSON response indicating success
-      res.status(200).json({ success: true, message: 'Pitch, roll, and yaw data received successfully' });
+      res.status(200).json({ success: true, message: 'Stingray data received successfully', id });
     } else {
       // Handle unsupported HTTP methods with an error response
       res.status(400).json({ success: false, message: 'Invalid HTTP method' });
