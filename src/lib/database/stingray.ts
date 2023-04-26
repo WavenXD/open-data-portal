c945a145a1c7877a1fa3967b4268474c257f1c56
WavenXD <artursbiten@gmail.com>
2023-04-26 13:20:31 +0200

    hardcoded insert

--- src/lib/database/stingray.ts
+++ src/lib/database/stingray.ts
@@ -25,7 +25,7 @@ export const createOne = async ({
         const result = await connection.query(
             `
       INSERT INTO stingray (long, lat, pitch, roll, time, temperature)
-      VALUES (?, ?, ?, ?, ?, ?)
+      VALUES (0, 0, 0.18214880260601957, 8.44751286450819, 2023-04-26T13:01:20.439039, 21.5)
   `,
             [long, lat, pitch, roll, time, temperature]
         );
